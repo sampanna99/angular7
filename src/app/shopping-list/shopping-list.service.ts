eb98201a3a562a57454fb16eea412e1f79233e17
Sampanna Pokhrel <sampannapokhrel@gmail.com>
2019-07-27 15:02:01 -0400

    observables subjects operators

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,11 @@
 import { Ingredient } from '../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService{
-    ingredientsChanged = new EventEmitter<Ingredient[]>()
+    ingredientsChanged = new Subject<Ingredient[]>()
+    // ingredientsChanged = new EventEmitter<Ingredient[]>()
+
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -14,10 +17,10 @@ export class ShoppingListService{
       }
       addIngredient(ingredient: Ingredient){
           this.ingredients.push(ingredient);
-          this.ingredientsChanged.emit(this.ingredients.slice());
+          this.ingredientsChanged.next(this.ingredients.slice());
       }
       addIngredients(ingredientst: Ingredient[]){
         this.ingredients.push(...ingredientst);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
       }
 }
