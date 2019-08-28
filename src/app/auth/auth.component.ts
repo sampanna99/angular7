c4bb0849b6228ed9bb93009a99c8743da1e78da3
Sampanna Pokhrel <sampannapokhrel@gmail.com>
2019-08-27 22:14:37 -0400

    sign up and spinner httpclient

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-auth',
@@ -7,12 +8,37 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent{
     isLoginMode = true;
+    isLoading = false;
+    error: string = null;
+
+    constructor(private authService: AuthService){
+
+    }
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode;
     }
     onSubmit(form: NgForm){
+        if(!form.valid){
+            return;
+        }
         console.log(form.value);
+        const email = form.value.email;
+        const password = form.value.password;
+        this.isLoading = true;
+        if(this.isLoginMode){
+
+        }else{
+            this.authService.signup(email, password).subscribe(resData => {
+                console.log(resData);
+                this.isLoading = false;
+            }, error => {
+                console.log(error);
+                this.error = "An error occured!";
+                this.isLoading = false;
+            });
+        }
+
         form.reset();
     }
 }
