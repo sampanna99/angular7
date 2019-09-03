ffe273f50d2cce626f6cbde6de0e122916ab7654
Sampanna Pokhrel <sampannapokhrel@gmail.com>
2019-09-03 19:39:23 -0400

    auto logout feature

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
     user = new BehaviorSubject<User>(null);
+    private tokenExpirationTimer: any;
     // user = new Subject<User>();
 
     // token: string = null;
@@ -70,20 +71,37 @@ export class AuthService {
 
         if (loadedUser.token){
             this.user.next(loadedUser);
-        }
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
 
+            this.autologout(expirationDuration);
+        }
     }
+
 
     
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if (this.tokenExpirationTimer){
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
+    }
+
+    autologout(expirationDuration: number){
+        console.log(expirationDuration);
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
+
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        this.autologout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
 
